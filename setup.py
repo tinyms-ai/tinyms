6a34f8719e4fd7e492a2f76ed246d89227506f66
jingyangxiang <jingyangxiang@foxmail.com>
2021-04-21 13:58:03 +0800

    bert

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ def _write_version(file):
 required_package = [
     'easydict >= 1.9',
     'scipy >= 1.5.3',
-    'mindspore == 1.1.1',
+    'mindspore == 1.2.0',
     'matplotlib >= 3.1.1',
     'Pillow >= 6.2.0',
     'requests >= 2.22.0',
