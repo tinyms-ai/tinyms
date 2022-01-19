c232faa46a999ddf9a5e148e780ef6b0f61651bb
helloway <simple_hlw@163.com>
2022-01-19 17:17:10 +0800

    update package import in module layer for adapting to MindSpore v1.6.0

--- tinyms/layers.py
+++ tinyms/layers.py
@@ -19,33 +19,16 @@ The high-level components (Layers) used to construct the neural network.
 """
 
 from mindspore.nn import Cell, GraphCell
-from mindspore.nn.layer.container import SequentialCell, CellList
+from mindspore.nn.layer import SequentialCell, CellList
 from mindspore.nn import wrap
 from mindspore.nn.wrap import *
-from mindspore.nn.layer import activation, normalization, conv, lstm, basic, \
-    embedding, pooling, math as nn_math, combined
-from mindspore.nn.layer.activation import *
-from mindspore.nn.layer.normalization import *
-from mindspore.nn.layer.conv import *
-from mindspore.nn.layer.lstm import *
-from mindspore.nn.layer.basic import *
-from mindspore.nn.layer.embedding import *
-from mindspore.nn.layer.pooling import *
-from mindspore.nn.layer.math import *
-from mindspore.nn.layer.combined import *
+from mindspore.nn import layer
+from mindspore.nn.layer import *
 
 
 __all__ = ['Layer', 'SequentialLayer', 'LayerList']
-__all__.extend(activation.__all__)
-__all__.extend(normalization.__all__)
-__all__.extend(conv.__all__)
-__all__.extend(lstm.__all__)
-__all__.extend(basic.__all__)
-__all__.extend(embedding.__all__)
-__all__.extend(pooling.__all__)
-__all__.extend(nn_math.__all__)
-__all__.extend(combined.__all__)
 __all__.extend(wrap.__all__)
+__all__.extend(layer.__all__)
 
 
 class Layer(Cell):
